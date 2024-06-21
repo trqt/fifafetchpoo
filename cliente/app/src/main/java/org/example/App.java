5fa3defc7fcb7867ca8ded3dda2601807d5f24a1
Guilherme Torquato <gtorquato@proton.me>
2024-06-21 18:23:44 +0000

    começo java

---
+++ cliente/app/src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import javax.swing.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Interface");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame
+        frame.
+    }
+}
